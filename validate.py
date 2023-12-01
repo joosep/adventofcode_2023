6f87fcc0959fb993579b8d09ea0839ef0ee202e2
Joosep <joosep@reach-u.com>
2023-12-01 23:03:12 +0200

    day1

--- validate.py
+++ validate.py
@@ -1,13 +1,13 @@
 def validate(fun, input_file, expected=None) -> bool:
-    print('========================================')
-    print(f'Testing: {fun.__name__}({input_file})')
     result = fun(input_file)
     if result != expected:
+        print('========================================')
         if expected is not None:
             print('Expected:')
             print(expected)
         print('Got:')
         print(result)
-        exit(1)
-    else:
-        print("OK!")
+        print(f'Tested: {fun.__name__}({input_file})')
+        print('========================================')
+        return False
+    return True
